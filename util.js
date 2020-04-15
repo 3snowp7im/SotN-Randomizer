915323c1c5e38fee2ded2d871c637508929975ae
Wild Mouse <3snowp7im@gmail.com>
2020-04-15 03:07:11 -0700

    Fix convenience url parsing at command line

--- util.js
+++ util.js
@@ -1366,6 +1366,9 @@
     const presets = Object.getOwnPropertyNames(constants.optionsUrls)
     for (let i = 0; i < presets.length; i++) {
       if (constants.optionsUrls[presets[i]] === baseUrl) {
+        if (args.length === 1) {
+          args.unshift(undefined)
+        }
         args.unshift(presets[i])
         break
       }
