0a7f07b25db8468484e87f9dc5d120d5829cf6fa
Wild Mouse <3snowp7im@gmail.com>
2020-04-29 15:41:04 -0700

    Add Pixie's song when sitting in a chair

--- randomize_relics.js
+++ randomize_relics.js
@@ -636,43 +636,47 @@
   }
 
   function patchPixieSong(data) {
-    // This code doesn't work.
-    // TODO: Fix this.
     const romAddress = 0x158fe8
     const ramAddress = 0x136f50
     let offset
     data.writeWord(0x0fe010, 0x08000000 + (ramAddress >> 2))
     offset = romAddress
-    offset = data.writeWord(offset, 0x3c088007) // lui t0, 0x8007
-    offset = data.writeWord(offset, 0x8509342e) // lh t1, 0x342e (t0)
-    offset = data.writeWord(offset, 0x340a00df) // ori t2, r0, 0x00df
-    offset = data.writeWord(offset, 0x152a000d) // bne t1, t2, pc + 0x38
+    offset = data.writeWord(offset, 0x27bdffe8) // addiu sp, sp, 0xffe8
+    offset = data.writeWord(offset, 0x3c1f8013) // lui ra, 0x8013
+    offset = data.writeWord(offset, 0x37ff6f68) // ori ra, ra, 0x6f68
+    offset = data.writeWord(offset, 0xafbf0010) // sw ra, 0x0010 (sp)
+    offset = data.writeWord(offset, 0x00400008) // jr v0
     offset = data.writeWord(offset, 0x00000000) // nop
-    offset = data.writeWord(offset, 0x850936ce) // lh t1, 0x36ce (t0)
-    offset = data.writeWord(offset, 0x340a0089) // ori t2, r0, 0x0089
-    offset = data.writeWord(offset, 0x152a0009) // bne t1, t2, pc + 0x28
-    offset = data.writeWord(offset, 0x00000000) // nop
+    offset = data.writeWord(offset, 0x8fbf0010) // lw ra, 0x0010 (sp)
+    offset = data.writeWord(offset, 0x27bd0018) // addiu sp, sp, 0x0018
     offset = data.writeWord(offset, 0x3c088009) // lui t0, 0x8009
     offset = data.writeWord(offset, 0x8109797b) // lb t1, 0x797b (t0)
     offset = data.writeWord(offset, 0x340a0003) // ori t2, r0, 0x0003
-    offset = data.writeWord(offset, 0x152a0004) // bne t1, t2, pc + 0x14
+    offset = data.writeWord(offset, 0x152a0017) // bne t1, t2, pc + 0x60
     offset = data.writeWord(offset, 0x00000000) // nop
     offset = data.writeWord(offset, 0x3c088007) // lui t0, 0x8007
-    offset = data.writeWord(offset, 0x3409738c) // ori t1, r0, 0x738c
-    offset = data.writeWord(offset, 0xa50936f0) // sh t1, 0x36f0 (t0)
-    offset = data.writeWord(offset, 0x3c088009) // lui t0, 0x8009
-    offset = data.writeWord(offset, 0x850974a0) // lh t1, 0x74a0 (t0)
-    offset = data.writeWord(offset, 0x340a0002) // ori t2, r0, 0x0002
-    offset = data.writeWord(offset, 0x152a0008) // bne t1, t2, pc + 0x24
+    offset = data.writeWord(offset, 0x81093714) // lb t1, 0x3714 (t0)
+    offset = data.writeWord(offset, 0x340a0034) // ori t2, r0, 0x0034
+    offset = data.writeWord(offset, 0x152a0012) // bne t1, t2, pc + 0x4c
     offset = data.writeWord(offset, 0x00000000) // nop
     offset = data.writeWord(offset, 0x3c088014) // lui t0, 0x8014
     offset = data.writeWord(offset, 0x85098458) // lh t1, 0x8458 (t0)
-    offset = data.writeWord(offset, 0x340a0104) // ori t2, r0, 0x0104
-    offset = data.writeWord(offset, 0x152a0003) // bne t1, t2, pc + 0x10
+    offset = data.writeWord(offset, 0x340a003f) // ori t2, r0, 0x003f
+    offset = data.writeWord(offset, 0x112a000d) // beq t1, t2, pc + 0x38
     offset = data.writeWord(offset, 0x00000000) // nop
-    offset = data.writeWord(offset, 0x3409003f) // ori t1, r0, 0x003f
-    offset = data.writeWord(offset, 0xa5098458) // sh t1, 0x8458 (t0)
-    offset = data.writeWord(offset, 0x00400008) // jr v0
+    offset = data.writeWord(offset, 0xa50a9064) // sh t2, 0x9064 (t0)
+    offset = data.writeWord(offset, 0x340a0001) // ori t2, r0, 0x0001
+    offset = data.writeWord(offset, 0xa50a96f4) // sh t2, 0x96f4 (t0)
+    offset = data.writeWord(offset, 0x340a0004) // ori t2, r0, 0x0004
+    offset = data.writeWord(offset, 0xa50a9868) // sh t2, 0x9868 (t0)
+    offset = data.writeWord(offset, 0x3c08800e) // lui t0, 0x800e
+    offset = data.writeWord(offset, 0x3c0a8013) // lui t2, 0x8013
+    offset = data.writeWord(offset, 0x354a2f9c) // ori t2, t2, 0x2f9c
+    offset = data.writeWord(offset, 0xad0a2034) // sw t2, 0x2034 (t0)
+    offset = data.writeWord(offset, 0x3c0a8013) // lui t2, 0x8013
+    offset = data.writeWord(offset, 0x354a5404) // ori t2, t2, 0x5404
+    offset = data.writeWord(offset, 0xad0a2364) // sw t2, 0x2364 (t0)    
+    offset = data.writeWord(offset, 0x03e00008) // jr ra
     offset = data.writeWord(offset, 0x00000000) // nop
   }
 
@@ -1043,7 +1047,7 @@
       case constants.EXTENSION.EQUIPMENT:
       case constants.EXTENSION.GUARDED:
         patchRelicsMenu(data)
-        //patchPixieSong(data)
+        patchPixieSong(data)
       }
     }
     return {
