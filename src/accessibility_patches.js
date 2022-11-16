0025a70f03c960701ced03a2b81ea0b5fa11b191
Talic <talic.zealot@gmail.com>
2022-11-16 02:02:22 +0200

    change accessibility-patches option to disable-accessibility-patches, fix indentation in accessibility_patches,.js

--- src/accessibility_patches.js
+++ src/accessibility_patches.js
@@ -1,29 +1,29 @@
-(function(self) {
-    let util
-  
-    if (self) {
-      util = self.sotnRando.util
-    } else {
-      util = require('./util')
-    }
+(function (self) {
+  let util
 
-    function patchPowerOfSireFlashing(data) {
-        // Fix researched by MottZilla.
-        data.writeWord(0x00136580, 0x03e00008)
-      }
-  
-    function applyAccessibilityPatches() {
-      const data = new util.checked()
-      patchPowerOfSireFlashing(data)
-      return data
-    }
-  
-    const exports = applyAccessibilityPatches
-    if (self) {
-      self.sotnRando = Object.assign(self.sotnRando || {}, {
-        applyAccessibilityPatches: exports,
-      })
-    } else {
-      module.exports = exports
-    }
-  })(typeof(self) !== 'undefined' ? self : null)
+  if (self) {
+    util = self.sotnRando.util
+  } else {
+    util = require('./util')
+  }
+
+  function patchPowerOfSireFlashing(data) {
+    // Fix researched by MottZilla.
+    data.writeWord(0x00136580, 0x03e00008)
+  }
+
+  function applyAccessibilityPatches() {
+    const data = new util.checked()
+    patchPowerOfSireFlashing(data)
+    return data
+  }
+
+  const exports = applyAccessibilityPatches
+  if (self) {
+    self.sotnRando = Object.assign(self.sotnRando || {}, {
+      applyAccessibilityPatches: exports,
+    })
+  } else {
+    module.exports = exports
+  }
+})(typeof (self) !== 'undefined' ? self : null)
